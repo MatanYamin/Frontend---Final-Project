2939e66d8a994c9c10f7bf22796a854c5bbc4907
MatanYamin <yamin2211@gmail.com>
2021-03-05 16:45:32 +0200

    Displaying navbar without design to the website

--- skycleaner-website/src/App.js
+++ skycleaner-website/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1> HI</h1>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+      </Router>
     </div>
   );
 }
--- skycleaner-website/src/components/Navbar.js
+++ skycleaner-website/src/components/Navbar.js
@@ -1,15 +1,17 @@
 // Matan Yamin - Frontend: Final Project.
 // will be the navbar component
 
-import React, {useState} from 'react'
-
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
     return (
         <>
         <nav className="navbar">
             <div className="navbar-container">
-                
+                <Link to="/" className="Navbar-logo"> 
+                Check Matan
+                </Link>
             </div>
 
         </nav>
