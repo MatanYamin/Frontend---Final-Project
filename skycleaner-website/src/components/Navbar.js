37e05ec5330035b8fb0cc1f4905450dbab70f2cb
MatanYamin <yamin2211@gmail.com>
2021-03-05 17:17:27 +0200

    continuing navbar

--- skycleaner-website/src/components/Navbar.js
+++ skycleaner-website/src/components/Navbar.js
@@ -3,15 +3,23 @@
 
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
-
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);  // reverse the false and true every time we click
     return (
+        //Will be the upper part including hamburger menu
         <>
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="Navbar-logo"> 
-                Check Matan
+                Check Matansa <i className="fab fa-typo3" />
                 </Link>
+                {/*  when pressing the icon*/}
+                <div className="menu-icon" onClick={handleClick}> 
+                    {/*  move bewtween images ervery time we click*/}
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> 
+                </div>
             </div>
 
         </nav>
