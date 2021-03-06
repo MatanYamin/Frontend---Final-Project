29f3758d4def4a64cf8036ab8dadacf38481e4a1
MatanYamin <yamin2211@gmail.com>
2021-03-06 21:31:02 +0200

    adding Middle component

---
+++ skycleaner-website/src/components/MiddleSection.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './MiddleSection.css';
+import {Link} from 'react-router-dom';
+
+function MiddleSection() {
+    return(
+        <div className="middle-container">
+            
+
+        </div>
+    )
+}
+
+export default MiddleSection
