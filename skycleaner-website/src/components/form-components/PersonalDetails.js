60e5b7beea5c8c6ea4213eb72685da2891d4fdf9
MatanYamin <yamin2211@gmail.com>
2021-03-08 15:30:01 +0200

    Seucceeded to pass page name for later use in order to fetch the page data from DB

--- skycleaner-website/src/components/form-components/PersonalDetails.js
+++ skycleaner-website/src/components/form-components/PersonalDetails.js
@@ -1,4 +1,6 @@
 import React from "react"
+import TextField from "../../../node_modules/@material-ui/core/TextField"
+
 
 function PersonalDetails() {
     return(
--- skycleaner-website/src/components/form-components/PickService.js
+++ skycleaner-website/src/components/form-components/PickService.js
@@ -1,8 +1,7 @@
 import React from "react"
+import { Component } from "react"
 import TextField from "../../../node_modules/@material-ui/core/TextField"
 import RaisedButton from "../../../node_modules/@material-ui/core/Button"
-import { Component } from "react"
-
 
 
 export default class PickService extends Component {
@@ -13,52 +12,27 @@ export default class PickService extends Component {
     }
     render() {
     const {values} = this.props; //values is all the props we passed to the component
-    switch(window.location.pathname){
-        case "/Clean1":
-            return(
-                <h1>CLEAN1</h1>
-            )
-        case "/Clean2":
-            return(
-                <h1>CLEAN2</h1>
-            )
-        case "/Clean3":
-            return(
-                <h1>CLEAN3</h1>
-            )
-        case "/Clean4":
-            return(
-                <h1>CLEAN4</h1>
-            )
-        }
-    // return(
-    //     <div>
-
-    //         {console.log(window.location.pathname)}
-    //         {/* {window.location.pathname.includes("Clean1") ? console.log("Clean1") : console.log("hi matan")} */}
-
-
-
-            
-    //         <br/>
-    //         <h5>בחר שרות</h5>
-    //         <br/>
-    //         <TextField 
-    //         hintText="asdasd"
-    //         floatingLabelText="בדיקה"
-    //         onChange={this.props.handleChange('service')}
-    //         defaultValue={values.service}
-    //         />
-    //         <br/><br/>
-    //         <h5>בחר תוספים</h5>
-    //         <br/>
-    //         <TextField 
-    //         hintText="הכנס תשובה"
-    //         floatingLabelText="בדיקה"
-    //         onChange={this.props.handleChange('addons')}
-    //         defaultValue={values.addons}
-    //         />
-    //     </div>
-    // );
+    const page = window.location.pathname.substring(1); //page name
+    return(
+        <div>
+            <h1>{page}</h1>
+            <br/>
+            <h5>בחר שרות</h5>
+            <br/>
+            <TextField 
+            placeholder="תשובה"
+            onChange={this.props.handleChange('service')}
+            defaultValue={values.service}
+            />
+            <br/><br/>
+            <h5>בחר תוספים</h5>
+            <br/>
+            <TextField
+            placeholder="תשובה"
+            onChange={this.props.handleChange('addons')}
+            defaultValue={values.addons}
+            />
+        </div>
+    );
         }
             }
