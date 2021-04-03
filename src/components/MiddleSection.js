0ca4e917c6297907b7144a778f4e84d0bf7ef37e
MatanYamin <yamin2211@gmail.com>
2021-04-03 19:08:18 +0300

    check change

--- src/components/MiddleSection.js
+++ src/components/MiddleSection.js
@@ -23,7 +23,7 @@ function MiddleSection() {
             <img class="first-img-logo" src="https://i.ibb.co/yh7CyXp/Sky-Cleaner.jpg" />
             <Popup trigger={<a class="button1">צפו בהגרלות שלנו!</a>} >
                 <div class="pop-up-content-home">
-                    <a class="video-intro">צפו בסרטון ההגרלה האחרונה שנערכה והישארו מעודכנים אחר הגרלות נוספות </a>
+                    <a class="video-intro">צפו בסרטון ההגרלה האחרונה שנערכה הישארו מעודכנים אחר הגרלות נוספות </a>
                     <br/>
                     <iframe width="560" height="415" src="https://www.youtube.com/embed/IPMN-LqzB7Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
